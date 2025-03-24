47c836a8e60f7af8f67c45a0a7bea090da8e69e1
solji ju <soljiju@gmail.com>
2025-03-24 16:36:24 +0900

    수정

---
+++ src/test/java/kr/co/soljiju/SpringGithubActionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.soljiju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
